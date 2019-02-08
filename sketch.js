15bf870f4dc823d674def2327459576dc619dbd7
SimplexShotz <44213379+SimplexShotz@users.noreply.github.com>
2019-02-07 20:04:19 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,30 +1,29 @@
 
-var fb = (function() {
-  var config = {
-    apiKey: "AIzaSyC82qKSxASHV73ab3MhmfZOw1OUryZwEqI",
-    authDomain: "realtimefirebasetest.firebaseapp.com",
-    databaseURL: "https://realtimefirebasetest.firebaseio.com",
-    projectId: "realtimefirebasetest",
-    storageBucket: "realtimefirebasetest.appspot.com",
-    messagingSenderId: "628446710214"
-  };
-  firebase.initializeApp(config);
-  var database = firebase.database();
-  return function() {
-    this.get = async function(r) {
-      var ret;
-      await database.ref(r).once("value", function(data) {
-        ret = data.val();
-      });
-      return ret;
-    };
-  };
-})();
+var fb;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-  // Initialize Firebase
-  
+  fb = (function() {
+    var config = {
+      apiKey: "AIzaSyC82qKSxASHV73ab3MhmfZOw1OUryZwEqI",
+      authDomain: "realtimefirebasetest.firebaseapp.com",
+      databaseURL: "https://realtimefirebasetest.firebaseio.com",
+      projectId: "realtimefirebasetest",
+      storageBucket: "realtimefirebasetest.appspot.com",
+      messagingSenderId: "628446710214"
+    };
+    firebase.initializeApp(config);
+    var database = firebase.database();
+    return function() {
+      this.get = async function(r) {
+        var ret;
+        await database.ref(r).once("value", function(data) {
+          ret = data.val();
+        });
+        return ret;
+      };
+    };
+  })();
 }
 
 function draw() {
